b377158790e25b0a55c17f474dabb1214e7bd30c
Ethan Hall <ethan@hall.sh>
2023-12-30 14:26:42 -0800

    Add docs

---
+++ examples/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S cargo run --bin pity-intercept -- --extra-config examples bash
+
+echo "hello world"
+echo "error 1!"
+exit 1
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/Users/ethan/workspace/ethankhall/pity/target/debug/pity-intercept -ddd bash
-#!/usr/bin/env bash
-
-echo "hello world"
-echo "error 1!"
-exit 1
