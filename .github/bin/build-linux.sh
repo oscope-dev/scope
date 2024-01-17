b26c446604c669afbd5ca5cfbcf691e171cc63a5
Ethan Hall <ethan@hall.sh>
2024-01-16 17:42:57 -0800

    Build Linux Artifacts

---
+++ .github/bin/build-linux.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+VERSION="${1:-'0.1.1-SNAPSHOT'}"
+
+cross build --target aarch64-unknown-linux-gnu --release --config package.version=\"${VERSION}\"
+cross build --target x86_64-unknown-linux-gnu --release --config package.version=\"${VERSION}\"
+
+echo "Built artifacts for amd64"
+file target/x86_64-unknown-linux-gnu/release/scope
+file target/x86_64-unknown-linux-gnu/release/scope-intercept
+
+echo "Built artifacts for aarch64"
+file target/aarch64-unknown-linux-gnu/release/scope
+file target/aarch64-unknown-linux-gnu/release/scope-intercept
+
+rm -rf target/x86_64-unknown-linux-gnu/artifact || true
+mkdir -p target/x86_64-unknown-linux-gnu/artifact
+
+cp target/x86_64-unknown-linux-gnu/release/scope target/x86_64-unknown-linux-gnu/artifact
+cp target/x86_64-unknown-linux-gnu/release/scope-intercept target/x86_64-unknown-linux-gnu/artifact
+
+rm -rf target/aarch64-unknown-linux-gnu/artifact || true
+mkdir -p target/aarch64-unknown-linux-gnu/artifact
+cp target/aarch64-unknown-linux-gnu/release/scope target/aarch64-unknown-linux-gnu/artifact
+cp target/aarch64-unknown-linux-gnu/release/scope-intercept target/aarch64-unknown-linux-gnu/artifact
