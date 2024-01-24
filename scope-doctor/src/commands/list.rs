8a1fed716415d12614b622ed402518b27ee972e4
Josh Nichols <josh.nichols@gusto.com>
2024-01-24 12:48:02 -0500

    Left-align output of doctor list

--- scope-doctor/src/commands/list.rs
+++ scope-doctor/src/commands/list.rs
@@ -9,12 +9,12 @@ pub struct DoctorListArgs {}
 
 pub async fn doctor_list(found_config: &FoundConfig, _args: &DoctorListArgs) -> Result<()> {
     info!(target: "user", "Available checks that will run");
-    info!(target: "user", "{:^20}{:^40}", "Name".white().bold(), "Description".white().bold());
+    info!(target: "user", "{:<20}{:<40}", "Name".white().bold(), "Description".white().bold());
     for check in found_config.doctor_exec.values() {
-        info!(target: "user", "{:^20}{:^40}", check.name().white().bold(), check.spec.description());
+        info!(target: "user", "{:<20}{:<40}", check.name().white().bold(), check.spec.description());
     }
     for check in found_config.doctor_setup.values() {
-        info!(target: "user", "{:^20}{:^40}", check.name().white().bold(), check.spec.description());
+        info!(target: "user", "{:<20}{:<40}", check.name().white().bold(), check.spec.description());
     }
     Ok(())
 }
