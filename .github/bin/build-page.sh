52f597bdd856efbb2d67d8a9c45ffd58ee5dbbae
Ethan Hall <ethan@hall.sh>
2024-01-23 20:58:39 -0800

    remove chmod

--- .github/bin/build-page.sh
+++ .github/bin/build-page.sh
@@ -3,4 +3,3 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker run -v "${SCRIPT_DIR}/../../docs:/app/docs" --entrypoint sh --workdir /app/docs node:20-buster build.sh
-chown -R "$(id)" "${SCRIPT_DIR}/../../docs/build"
