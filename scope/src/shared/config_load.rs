bd66335dbd98b06cb3a1246c4a2da4f0eb4ef818
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2024-03-21 08:42:36 -0700

    Fix: linters

--- scope/src/shared/config_load.rs
+++ scope/src/shared/config_load.rs
@@ -8,10 +8,10 @@ use anyhow::{anyhow, Result};
 use clap::{ArgGroup, Parser};
 use colored::*;
 use directories::{BaseDirs, UserDirs};
+use ignore::Walk;
 use itertools::Itertools;
 use serde::Deserialize;
 use serde_yaml::{Deserializer, Value};
-use ignore::Walk;
 
 use std::collections::BTreeMap;
 use std::ffi::OsStr;
@@ -317,7 +317,7 @@ fn expand_path(path: &Path) -> Result<Vec<PathBuf>> {
 
     if path.is_dir() {
         let mut files = Vec::new();
-        for dir_entry in Walk::new(path).into_iter().filter_map(|e| e.ok()) {
+        for dir_entry in Walk::new(path).filter_map(|e| e.ok()) {
             if !dir_entry.path().is_file() {
                 continue;
             }
