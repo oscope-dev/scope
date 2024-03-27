d313632055d84c733b50debf3b33040d7c02f57f
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2024-03-27 08:47:57 -0700

    Fix: handle missing shared directory

--- scope/src/shared/config_load.rs
+++ scope/src/shared/config_load.rs
@@ -141,13 +141,13 @@ impl FoundConfig {
 
         let mut config_path = config_path.to_vec();
         let exe_path = std::env::current_exe().unwrap();
-        let shared_path = exe_path
-            .parent()
-            .unwrap()
-            .join("../etc/scope")
-            .canonicalize()
-            .expect("shared path to be canonicalizable");
-        config_path.push(shared_path);
+        let shared_path = exe_path.parent().unwrap().join("../etc/scope");
+        if shared_path.exists() {
+            let can_path = shared_path
+                .canonicalize()
+                .expect("shared path to be canonicalizable");
+            config_path.push(can_path);
+        }
 
         let scope_path = config_path
             .iter()
